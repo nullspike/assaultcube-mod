32477e6542974b4a924fbdb931a7510fbc54f44d
nullspike <nullspike@localhost>
2023-10-01 04:32:56 +0300

    nopAt

--- Ac Mod/mem.cpp
+++ Ac Mod/mem.cpp
@@ -6,5 +6,10 @@ void mem::patchAt(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProc) {
 
 
 void mem::nopAt(BYTE* dst, unsigned int size, HANDLE hProc) {
-
+	DWORD oldPermissions;
+	VirtualProtectEx(hProc, dst, size, PAGE_EXECUTE_READWRITE, &oldPermissions);
+	BYTE* nops = new BYTE[size]; 
+	memset(nops, 0x90, size);
+	WriteProcessMemory(hProc, dst, nops, size, nullptr);
+	VirtualProtectEx(hProc, dst, size, oldPermissions, &oldPermissions);
 }
